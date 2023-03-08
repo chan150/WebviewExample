d01b43a92c09430d4c710935acfd9dcc393e35e3
Himchan Park <chan150@kaist.ac.kr>
2023-03-08 16:05:07 +0900

    Web에서 제한이 있음

--- lib/main.dart
+++ lib/main.dart
@@ -59,24 +59,24 @@ class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
   final controller = WebViewController()
-    ..setJavaScriptMode(JavaScriptMode.unrestricted)
-    ..setBackgroundColor(const Color(0x00000000))
-    ..setNavigationDelegate(
-      NavigationDelegate(
-        onProgress: (int progress) {
-          // Update loading bar.
-        },
-        onPageStarted: (String url) {},
-        onPageFinished: (String url) {},
-        onWebResourceError: (WebResourceError error) {},
-        onNavigationRequest: (NavigationRequest request) {
-          if (request.url.startsWith('https://www.youtube.com/')) {
-            return NavigationDecision.prevent;
-          }
-          return NavigationDecision.navigate;
-        },
-      ),
-    )
+    // ..setJavaScriptMode(JavaScriptMode.unrestricted)
+    // ..setBackgroundColor(const Color(0x00000000))
+    // ..setNavigationDelegate(
+    //   NavigationDelegate(
+    //     onProgress: (int progress) {
+    //       // Update loading bar.
+    //     },
+    //     onPageStarted: (String url) {},
+    //     onPageFinished: (String url) {},
+    //     onWebResourceError: (WebResourceError error) {},
+    //     onNavigationRequest: (NavigationRequest request) {
+    //       if (request.url.startsWith('https://www.youtube.com/')) {
+    //         return NavigationDecision.prevent;
+    //       }
+    //       return NavigationDecision.navigate;
+    //     },
+    //   ),
+    // )
     ..loadRequest(Uri.parse('https://flutter.dev'));
 
   void _incrementCounter() {
